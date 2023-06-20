064e6cadbe4648d7576139a937331608af8dd61b
Your Name <you@example.com>
2023-06-20 22:13:59 +0530

    adidng the full name part only

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 print('hello world ')
 print("hello wthere is this next line to print")
 print("adding a new line 3rd one ")
+
+a = "sravan "
+b = "kumar " 
+c = "reddy "
+d = "Palla "
+
+print("my full name is " + a + b + c + d )
+print("My full name as per the last name and frist name would " + " "+ d + a + b + c)
+
+
